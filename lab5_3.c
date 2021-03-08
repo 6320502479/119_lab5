5f5c2480805ae49816e7cc611171d1bdc070a097
6320502479 <pattrapol.J@ku.th>
2021-03-08 17:52:34 +0700

    g

--- lab5_3.c
+++ lab5_3.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 int main()
 {
-    int N,i[3]={2,3,5},k=1,num,j=0,re=2;
+    int N,i[3]={2,3,5},k=1,num,j=0,re=1;
     scanf("%d",&N);
     num=re;
-    while(k<=N)
+    while(k!=N)
     {
-        while(num!=1)
+        printf("%d %d \n",num,re);
+       while(num!=1)
         {
+            if(j>2)
+                {
+                    num++;
+                    j==0;
+                }
             if(num%i[j]==0)
             {
                 num/=i[j];
             }
             else
-            {
                 j++;
-                if(j>2)
-                {
-                    re++;
-                    break;
-                }
-
-            }
         }
         if(num==1)
         {
             k++;
             re++;
+            num=re;
         }
         j==0;
+
     }
     printf("%d",re);
 }
